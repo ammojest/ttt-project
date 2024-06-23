925b4633f9544fb6a32b6d05fa90a23b9ff18ea6
Tom James <63594225+ammojest@users.noreply.github.com>
2024-06-23 16:28:36 +0100

    chore: Add Square and Board components for tic-tac-toe game

---
+++ src/app/components/Board.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState } from "react";
+import Square from "./Square";
+
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i: number) {
+    if (squares[i] || calculateWinner(squares)) {
+      alert("You're the winner");
+      return;
+    }
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  function calculateWinner(squares: any[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const resetBoard = () => {
+    setSquares(Array(9).fill(null));
+  };
+  return (
+    <>
+      <div className="container w-fit">
+        <div className="flex">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="flex">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="flex">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+        <div className="flex justify-center my-5">
+          <button
+            onClick={resetBoard}
+            className="bg-blue-600 text-white px-5 py-2 rounded-xl"
+          >
+            reset
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Board;
---
+++ src/app/components/Square.tsx
@@ -0,0 +1,20 @@
+import React, { MouseEventHandler } from "react";
+
+const Square = ({
+  value,
+  onSquareClick,
+}: {
+  value: number;
+  onSquareClick: MouseEventHandler<HTMLButtonElement> | undefined;
+}) => {
+  return (
+    <button
+      onClick={onSquareClick}
+      className="bg-blue-900 border-black border-2 text-white text-2xl p-5 size-32"
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
